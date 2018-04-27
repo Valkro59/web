a71a93b03bcd4b23ba26533155642e4daa76388e
Valentin KROLAK <valkro59@gmail.com>
2018-04-27 15:31:54 +0200

    Finition website + ajout mini-JavaScript

---
+++ website/js/main.js
@@ -0,0 +1,9 @@
+// voir lien script dans html
+
+// alert est une commande js qui ici va s'ouvrir à chaque fois que la page s'ouvre
+
+var btn = document.getElementById('btnHello');
+btn.addEventListener('click', function()
+{alert('Hello World');
+}, false);
+
